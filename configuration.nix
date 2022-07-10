c2c07d5eb6811012a2ca66f0d670bf00ba9f25cc
Henry den Hengst <henrydenhengst@gmail.com>
2022-07-10 18:06:26 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -349,13 +349,13 @@
 
 
 ### clean system
-nix = {
-  settings.auto-optimise-store = true;
-  gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-};
+#nix = {
+#  settings.auto-optimise-store = true;
+#  gc = {
+#    automatic = true;
+#    dates = "weekly";
+#    options = "--delete-older-than 7d";
+#  };
+#};
 
 
