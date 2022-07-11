6afbc98712e02862b9aa2873298e0dbb7621e4dc
Henry den Hengst <henrydenhengst@gmail.com>
2022-07-11 12:23:43 +0200

    Create cinnamon-dump-process.sh
    backup cinnamon settings

---
+++ cinnamon-dump-process.sh
@@ -0,0 +1,8 @@
+# DUMP PROCESS
+dconf dump /org/cinnamon/ > cinnamon.dconf
+dconf dump /org/nemo/ > nemo.dconf
+dconf dump /org/gtk/ > gtk.dconf
+dconf dump /org/gnome/ > gnome.dconf
+
+# My CTRL+SHIFT +ESC for gnome-system-monitor, wallpaper, font, all cinnamon, nemo, gnome and gtk settings transferred with ease.
+# source Eric Dubois
