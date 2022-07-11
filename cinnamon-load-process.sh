554645d8786d43a9864d126ed59c4ad9f8d4565e
Henry den Hengst <henrydenhengst@gmail.com>
2022-07-11 12:25:38 +0200

    Create cinnamon-load-process.sh

---
+++ cinnamon-load-process.sh
@@ -0,0 +1,8 @@
+# LOAD PROCESS
+dconf load /org/cinnamon/ < cinnamon.dconf
+dconf load /org/nemo/ < nemo.dconf
+dconf load /org/gtk/ < gtk.dconf
+dconf load /org/gnome/ < gnome.dconf
+
+# My CTRL+SHIFT +ESC for gnome-system-monitor, wallpaper, font, all cinnamon, nemo, gnome and gtk settings transferred with ease.
+# Source Eric Dubois
