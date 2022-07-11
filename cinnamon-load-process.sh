26d1c735d79154c33e9bd303ecd540869a00f76b
Henry den Hengst <henrydenhengst@gmail.com>
2022-07-11 12:30:30 +0200

    Update cinnamon-load-process.sh

--- cinnamon-load-process.sh
+++ cinnamon-load-process.sh
@@ -4,5 +4,8 @@ dconf load /org/nemo/ < nemo.dconf
 dconf load /org/gtk/ < gtk.dconf
 dconf load /org/gnome/ < gnome.dconf
 
+# SET WALLPAPER
+gsettings set org.cinnamon.desktop.background picture-uri "file://<path to file>"
+
 # My CTRL+SHIFT +ESC for gnome-system-monitor, wallpaper, font, all cinnamon, nemo, gnome and gtk settings transferred with ease.
 # Source Eric Dubois
