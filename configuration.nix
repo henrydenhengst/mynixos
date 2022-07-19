74d7b202b8422f216c2aa7afc31c50607cd8fac4
Henry den Hengst <henrydenhengst@gmail.com>
2022-07-19 23:15:40 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -269,7 +269,6 @@
     ripgrep
     rsync # https://search.nixos.org/options?channel=22.05&from=0&size=50&sort=relevance&type=packages&query=rsync
     rustscan
-    shotcut
     smartmontools
     socialscan
     sqlmap
