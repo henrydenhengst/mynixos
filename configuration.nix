38ca4bb5a9553d8f6f1471bc8d66420b211fc420
Henry den Hengst <henrydenhengst@gmail.com>
2023-07-08 18:49:24 +0200

    Update configuration.nix
    MAJOR UPGRADE TO KDE

--- configuration.nix
+++ configuration.nix
@@ -11,72 +11,61 @@
       # <home-manager/nixos>
     ];
 
-  # Bootloader, check these hardware settings before you copy!
-  
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
 
   # Define your network
-    networking = { 
-      hostName = "oslo";
+    networking = {
+      hostName = "stavanger";
       domain = "viking.lan";
       search = [ "viking.lan" ];
       networkmanager.enable = true;
     };
 
   # Enables wireless support via wpa_supplicant.
-  # networking.wireless.enable = true;  
+  # networking.wireless.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "nl_NL.utf8";
-
-  # Enable the Cinnamon Desktop Environment.
-  services = {
-    xserver = {
-      enable = true;
-      layout = "us";
-      xkbVariant = "euro";
-      displayManager = {
-        lightdm.enable = true;
-        defaultSession = "cinnamon";
-      };
-      desktopManager.cinnamon.enable = true;
-      windowManager.awesome.enable = true;
-    };
+  console = {
+    packages=[ pkgs.terminus_font ];
+    font="${pkgs.terminus_font}/share/consolefonts/ter-i22b.psf.gz";
+    useXkbConfig = true; # use xkbOptions in tty.
   };
+
+  # Enable the KDE Plasma 5 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Enable CUPS to print documents for HP
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ hplip hplipWithPlugin ];    
-    # canon-cups-ufr2 carps-cups 
+    drivers = with pkgs; [ hplip hplipWithPlugin ];
+    # canon-cups-ufr2 carps-cups
     # gutenprint
   };
   programs.system-config-printer.enable = true;
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
-  
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.opengl.driSupport32Bit = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
     jack.enable = true;
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -89,13 +78,13 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
     mutableUsers = true;
-    users.henry = {
+    users.yourname = {
       isNormalUser = true;
-      description = "Henry den Hengst";
-      extraGroups = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner" "docker" "kvm" "wireshark" "vnstat" "lightdm" ];
+      description = "First Name - Last Name";
+      extraGroups = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner" "wireshark" "vnstat" "sddm" ];
       # You can keep the initialPassword if you *really* want to, but
       # that risks forgetting to change it
-      initialPassword = "password";
+      initialPassword = "ComplicatedPassword!!!";
     };
   };
 
@@ -105,13 +94,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    
-    ### GTK Themes
-    vimix-gtk-themes
-    stilo-themes
-    zuki-themes
-    dracula-theme
-    
+
     ### Fonts
     open-fonts
     nerdfonts
@@ -128,175 +111,49 @@
     roboto-mono
     ultimate-oldschool-pc-font-pack
     libre-caslon
-    
+
     ### awesome wm
-    alacritty
-    arandr
-    conky
-    dmenu
-    feh
-    librewolf
-    mpc-cli
-    mpc-qt
-    mpc123
-    mpd
-    ncmpcpp
-    neocomp
+    kitty
     neofetch
-    networkmanagerapplet
-    nitrogen
-    picom
-    polybar
-    scrot
-    slock
-    unclutter
-    volumeicon
-    xorg.xbacklight
-    xsel
-    
+
     ### Audio & Video
-    ardour
-    clementine
-    deadbeef
     freetube
     kdenlive
-    kodi
     lbry
     mpv
     obs-studio
-    droidcam
     vlc
     soundconverter
-    lmms
     kazam
     simplescreenrecorder
     handbrake
-    
-    ### Kodi Packages
-    kodiPackages.kodi
-    kodiPackages.six
-    kodiPackages.idna
-    kodiPackages.iagl
-    kodiPackages.trakt
-    kodiPackages.arrow
-    kodiPackages.kodi-six
-    kodiPackages.keymap
-    kodiPackages.future
-    kodiPackages.youtube
-    kodiPackages.urllib3
-    kodiPackages.svtplay
-    kodiPackages.signals
-    kodiPackages.routing
-    kodiPackages.pvr-hts
-    kodiPackages.netflix
-    kodiPackages.chardet
-    kodiPackages.certifi
-    kodiPackages.vfs-sftp
-    kodiPackages.requests
-    kodiPackages.myconnpy
-    kodiPackages.libretro
-    kodiPackages.joystick
-    kodiPackages.jellyfin
-    kodiPackages.dateutil
-    kodiPackages.websocket
-    kodiPackages.pdfreader
-    kodiPackages.osmc-skin
-    kodiPackages.orftvthek
-    kodiPackages.invidious
-    kodiPackages.xbmcswift2
-    kodiPackages.simplejson
-    kodiPackages.defusedxml
-    kodiPackages.kodi-platform
-    kodiPackages.trakt-module
-    kodiPackages.arteplussept
-    kodiPackages.archive_tool
-    kodiPackages.a4ksubtitles
-    kodiPackages.steam-library
-    kodiPackages.pvr-hdhomerun
-    kodiPackages.libretro-mgba
-    kodiPackages.vfs-libarchive
-    kodiPackages.steam-launcher
-    kodiPackages.requests-cache
-    kodiPackages.pvr-iptvsimple
-    kodiPackages.libretro-snes9x
-    kodiPackages.steam-controller
-    kodiPackages.libretro-genplus
-    kodiPackages.inputstream-rtmp
-    kodiPackages.typing_extensions
-    kodiPackages.inputstreamhelper
-    kodiPackages.inputstream-adaptive
-    kodiPackages.inputstream-ffmpegdirect
-    kodiPackages.controller-topology-project
 
     ### Developer Tools
-    # emacs   
+    # emacs
     vim
     geany
     micro
-    vscodium 
-    
-    ### Games
-    steam
-    retroarchFull
-    zeroad
-    xonotic
-    endless-sky
-    flare
-    astromenace
-    redeclipse
-    superTuxKart
-    lincity
-    extremetuxracer
-    urbanterror
-    speed_dreams
-    torcs
-    bottles
-    lutris
-    grapejuice # this is really Roblox !
-    
-    
+
     ### Graphics & Photography
     inkscape
     gimp
     blender
     darktable
     flameshot
-    
+
     ### Communication / Internet / News
     brave
     firefox
-    # google-chrome
     signal-desktop
-    element-desktop
-    discord
-    shortwave
-    slack
     lynx
-    # tor
-    # tor-browser-bundle-bin
-    # onionshare
-    
+
     ### Productivity
-    exodus
     ghostwriter
     libreoffice
     simplenote
     foliate
-    qalculate-gtk
-    monero-gui
-    
+
     ### Science / System / Utilities / Privacy / Security
-    # bpytop
-    # flatpak
-    # htop
-    # kitty
-    # malcontent     # parental control
-    # malcontent-ui  # parental control
-    # thunderbird
-    # wezterm
-    aircrack-ng
-    ansible # Deployment done right
-    appimage-run
     arp-scan
     asciinema
     atuin
@@ -317,8 +174,6 @@
     crunch
     curl
     dbus
-    dconf
-    dconf2nix
     dirb
     dmidecode
     dnsutils
@@ -330,13 +185,16 @@
     file
     filezilla
     fish
+    flatpak
     fundoc
+    gcc
     geekbench
     ghostscript
     git
     gnupg
+    gnugrep
+	  gnumake
     gparted
-    gphoto2
     hash-identifier
     hashcat
     hddtemp
@@ -346,7 +204,6 @@
     httrack
     iftop
     imagemagick
-    ipfs
     jmtpfs
     john
     keepassxc
@@ -380,7 +237,6 @@
     procs
     proxychains
     pulumi-bin # Infrastructure as code
-    pythonFull
     python3Full
     qbittorrent
     ranger
@@ -399,10 +255,8 @@
     tealdeer # tldr for various shell tools
     terminator
     thc-hydra
-    tmux 
-    tmuxPlugins.dracula
-    tmuxPlugins.resurrect
-    tmuxPlugins.tilish
+    tldr
+    tmux
     tokei
     tomb
     topgrade
@@ -429,40 +283,13 @@
     zip
     zola # Static site generator written in Rust
     zsh
-    
-    ### Virtualization
-    docker
-    docker-compose
-
   ];
-  
+
   ### List services that you want to enable:
-  
-  ### Kodi + Plugins
-  services.xserver.desktopManager.kodi.enable = true;
-  services.xserver.desktopManager.kodi.package = pkgs.kodi.withPackages (pkgs: with pkgs; [ osmc-skin ]);   
-  
-  ### RetroArch
-  services.xserver.desktopManager.retroarch.enable = true;
-  
-  ### Steam
-  programs.steam.enable = true;
-  
-  ### Droidcam
-  programs.droidcam.enable = true;
-  
-  ### parental control
-  # services.malcontent.enable = true;
-  
+
   ### wireshark
   programs.wireshark.enable = true;
-  
-  ### IPFS
-  services.ipfs.enable = true;
-  
-  ### Dconf
-  programs.dconf.enable = true;
-  
+
   ### Starship
   programs.starship.enable = true;
   programs.starship.settings = {
@@ -526,57 +353,54 @@
       error_symbol = "[\\$](bright-red bold)";
     };
   };
-  
+
   ### Git
   programs.git.enable = true;
-  
+
   ### Vim
   # programs.vim.package = true;
   programs.vim.defaultEditor = true;
-  
-  ### emacs
-  # services.emacs.enable = true;
-  # services.emacs.install = true;
-  
+
   ### vnstat
   services.vnstat.enable = true;
-  
+
   ### traceroute
-  programs.traceroute.enable = true;  
-  
+  programs.traceroute.enable = true;
+
   ### tmux
   programs.tmux.enable = true;
-  programs.tmux.plugins = with pkgs.tmuxPlugins; [
-    resurrect
-    tilish
-    dracula
-  ];
 
   ### bash
   programs.bash.enableCompletion = true;
   programs.bash.enableLsColors = true;
-  
+
   ### zsh
   programs.zsh.enable = true;
   # programs.zsh.enableCompletion = true;
   # programs.zsh.syntaxHighlighting.enable = true;
   # programs.zsh.autosuggestions.enable = true;
   # programs.zsh.ohMyZsh.enable = true;
-  
+
   ### fish
   programs.fish.enable = true;
   
+  # enable flatpak support
+  services.flatpak.enable = true;
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    # wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   ### Fonts Settings
   fonts.enableGhostscriptFonts = true;
-  fonts.fonts = with pkgs; [ open-fonts nerdfonts terminus-nerdfont inconsolata-nerdfont google-fonts powerline-fonts vistafonts corefonts redhat-official-fonts textfonts liberation_ttf freefont_ttf roboto-mono ultimate-oldschool-pc-font-pack libre-caslon ];
+  fonts.fonts = with pkgs; [ open-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji font-awesome nerdfonts terminus-nerdfont inconsolata-nerdfont google-fonts powerline-fonts vistafonts corefonts redhat-official-fonts textfonts liberation_ttf freefont_ttf roboto-mono ultimate-oldschool-pc-font-pack libre-caslon ];
   
-  ### Docker
-  virtualisation.docker.enable = true;
-  ## Default delivery with Kali Linux https://hub.docker.com/r/kalilinux/kali-rolling
-  ## https://www.kali.org/docs/containers/official-kalilinux-docker-images/
 
   ### Enable the OpenSSH daemon.
-  services.openssh.enable = true;  
+  services.openssh.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -595,8 +419,15 @@
   ### automatic upgrade
   system.autoUpgrade = {
       enable = true;
-      channel = "https://nixos.org/channels/nixos-22.05";
+      channel = "https://nixos.org/channels/nixos-23.05";
   };
+  
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+  system.autoUpgrade.allowReboot = true; 
+
 
   ### clean system
   nix = {
@@ -608,25 +439,6 @@
       options = "--delete-older-than 7d";
     };
   };
-
-  ### Home-Manager to install pkgs for a user, and configuration of dotfiles etc
-  # home-manager.user.henry = { pkgs, ... }: {
-  #   home.packages = with pkgs; [ htop vim ];
-  #   home.file = {
-  #     ".config/alacritty/alacritty.yml".txt = ' '
-  #       {"font":{bold":{"style":"Bold"}}}
-  #     ' ';
-  #   };
-  #   home.file.".doom.d" = {
-  #     source ./doom.d;
-  #     recursive = true;
-  #     onChange = buildins.readFile ./doom.sh;
-  #   };
-  #   home.file.".config/polybar/script/mic.sh"={
-  #     source = ./mic.sh;
-  #     executable = true;
-  #   };
-  # };
 
   ### Flakes
   nix = {
